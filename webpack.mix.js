4903922fc754b113e52eb388ff3a58b944264a3a
Ian Vital <373357042@qq.com>
2018-11-14 22:25:47 +0800

    webpack extract vue

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,7 @@ let mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/assets/js/app.js', 'public/js').version();
+mix.js('resources/assets/js/app.js', 'public/js').version().extract(['vue']);
 mix.sass('resources/assets/sass/app.scss', 'public/css')
    .styles([
         'resources/assets/css/animate.css',
